b01b338061fe14ca4f325932ee7c05e5852034d7
Carlos Zanotti <cezanotti@gmail.com>
2025-08-29 12:06:40 -0300

    Add favicon and apply aesthetic improvements to UI

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY:'scroll', border:'5px solid black', height:'500px'}}>
+        <div style={{overflowY:'scroll', borderTop:'3px solid #0206125a', height:'500px'}}>
             {props.children}
         </div>      
     )
